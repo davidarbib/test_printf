1c85e6dd2bc7f9b4360735d8e5b239415c15ff95
David ARBIB <darbib@e2r13p4.42.fr>
2019-04-11 14:52:16 +0200

    refine d_tests

---
+++ testft_printf.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   testft_printf.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darbib <darbib@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/11 14:00:54 by darbib            #+#    #+#             */
+/*   Updated: 2019/04/11 14:51:26 by darbib           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef TESTFT_PRINTF_H
+# define TESTFT_PRINTF_H
+
+#include "libftprintf.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void	d_tests();
+void	i_tests();
+void	o_tests();
+void	u_tests();
+void	x_tests();
+void	bigx_tests();
+void	c_tests();
+void	s_tests();
+void	p_tests();
+void	f_tests();
+void	true_d_tests();
+void	true_i_tests();
+void	true_o_tests();
+void	true_u_tests();
+void	true_x_tests();
+void	true_bigx_tests();
+void	true_c_tests();
+void	true_s_tests();
+void	true_p_tests();
+void	true_f_tests();
+#endif
---
+++ true_d_tests.c
@@ -0,0 +1,317 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   d_tests.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: darbib <darbib@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/05 15:02:58 by darbib            #+#    #+#             */
+/*   Updated: 2019/04/11 14:22:20 by darbib           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "testprintf.h"
+
+void d_tests()
+{
+	printf("%d\n",printf("%d\n", 42));
+	printf("%d\n",printf("%d\n", 0));
+	printf("%d\n",printf("%d\n", -42));
+	printf("%d\n",printf("%0d\n", 42));
+	printf("%d\n",printf("%0d\n", 0));
+	printf("%d\n",printf("%0d\n", -42));
+	printf("%d\n",printf("%#d\n", 42));
+	printf("%d\n",printf("%#d\n", 0));
+	printf("%d\n",printf("%#d\n", -42));
+	printf("%d\n",printf("%-d\n", 42));
+	printf("%d\n",printf("%-d\n", 0));
+	printf("%d\n",printf("%-d\n", -42));
+	printf("%d\n",printf("% d\n", 42));
+	printf("%d\n",printf("% d\n", 0));
+	printf("%d\n",printf("% d\n", -42));
+	printf("%d\n",printf("%+d\n", 42));
+	printf("%d\n",printf("%+d\n", 0));
+	printf("%d\n",printf("%+d\n", -42));
+	printf("%d\n",printf("% d\n", 42));
+	printf("%d\n",printf("% d\n", 0));
+	printf("%d\n",printf("% d\n", -42));
+	printf("%d\n",printf("%%d\n", 42));
+	printf("%d\n",printf("%%d\n", 0));
+	printf("%d\n",printf("%%d\n", -42));
+	printf("%d\n",printf("%%0d\n", 42));
+	printf("%d\n",printf("%%0d\n", 0));
+	printf("%d\n",printf("%%0d\n", -42));
+	printf("%d\n",printf("%%#d\n", 42));
+	printf("%d\n",printf("%%#d\n", 0));
+	printf("%d\n",printf("%%#d\n", -42));
+	printf("%d\n",printf("%%-d\n", 42));
+	printf("%d\n",printf("%%-d\n", 0));
+	printf("%d\n",printf("%%-d\n", -42));
+	printf("%d\n",printf("%% d\n", 42));
+	printf("%d\n",printf("%% d\n", 0));
+	printf("%d\n",printf("%% d\n", -42));
+	printf("%d\n",printf("%%+d\n", 42));
+	printf("%d\n",printf("%%+d\n", 0));
+	printf("%d\n",printf("%%+d\n", -42));
+	printf("%d\n",printf("%% d\n", 42));
+	printf("%d\n",printf("%% d\n", 0));
+	printf("%d\n",printf("%% d\n", -42));
+	printf("%d\n",printf("%d\n", 42));
+	printf("%d\n",printf("%d\n", 0));
+	printf("%d\n",printf("%d\n", -42));
+	printf("%d\n",printf("%0d\n", 42));
+	printf("%d\n",printf("%0d\n", 0));
+	printf("%d\n",printf("%0d\n", -42));
+	printf("%d\n",printf("%#d\n", 42));
+	printf("%d\n",printf("%#d\n", 0));
+	printf("%d\n",printf("%#d\n", -42));
+	printf("%d\n",printf("%-d\n", 42));
+	printf("%d\n",printf("%-d\n", 0));
+	printf("%d\n",printf("%-d\n", -42));
+	printf("%d\n",printf("% d\n", 42));
+	printf("%d\n",printf("% d\n", 0));
+	printf("%d\n",printf("% d\n", -42));
+	printf("%d\n",printf("%+d\n", 42));
+	printf("%d\n",printf("%+d\n", 0));
+	printf("%d\n",printf("%+d\n", -42));
+	printf("%d\n",printf("% d\n", 42));
+	printf("%d\n",printf("% d\n", 0));
+	printf("%d\n",printf("% d\n", -42));
+
+// -------- repetitive options ---------- 
+	printf("%d\n",printf("%++++++++++++++d\n", 42));
+	printf("%d\n",printf("%++++++++++++++d\n", 0));
+	printf("%d\n",printf("%++++++++++++++d\n", -42));
+	printf("%d\n",printf("%---------------d\n", 42));
+	printf("%d\n",printf("%---------------d\n", 0));
+	printf("%d\n",printf("%---------------d\n", -42));
+	printf("%d\n",printf("%###############d\n", 42));
+	printf("%d\n",printf("%###############d\n", 0));
+	printf("%d\n",printf("%###############d\n", -42));
+	printf("%d\n",printf("%00000000000000000000d\n", 42));
+	printf("%d\n",printf("%00000000000000000000d\n", 0));
+	printf("%d\n",printf("%00000000000000000000d\n", -42));
+	printf("%d\n",printf("%                    d\n", 42));
+	printf("%d\n",printf("%                     d\n", 0));
+	printf("%d\n",printf("%                     d\n", -42));
+
+// ---------- mixed options -----------------
+	printf("%d\n",printf("%########++++++++d\n", 42));
+	printf("%d\n",printf("%########++++++++d\n", 0));
+	printf("%d\n",printf("%########++++++++d\n", -42));
+	printf("%d\n",printf("%++++++++########d\n", 42));
+	printf("%d\n",printf("%++++++++########d\n", 0));
+	printf("%d\n",printf("%++++++++########d\n", -42));
+	printf("%d\n",printf("%########--------d\n", 42));
+	printf("%d\n",printf("%########--------d\n", 0));
+	printf("%d\n",printf("%########--------d\n", -42));
+	printf("%d\n",printf("%--------########d\n", 42));
+	printf("%d\n",printf("%--------########d\n", 0));
+	printf("%d\n",printf("%--------########d\n", -42));
+	printf("%d\n",printf("%########00000000d\n", 42));
+	printf("%d\n",printf("%########00000000d\n", 0));
+	printf("%d\n",printf("%########00000000d\n", -42));
+	printf("%d\n",printf("%00000000########d\n", 42));
+	printf("%d\n",printf("%00000000########d\n", 0));
+	printf("%d\n",printf("%00000000########d\n", -42));
+	printf("%d\n",printf("%########        d\n", 42));
+	printf("%d\n",printf("%########        d\n", 0));
+	printf("%d\n",printf("%########        d\n", -42));
+	printf("%d\n",printf("%        ########d\n", 42));
+	printf("%d\n",printf("%        ########d\n", 0));
+	printf("%d\n",printf("%        ########d\n", -42));
+	printf("%d\n",printf("%++++++++--------d\n", 42));
+	printf("%d\n",printf("%++++++++--------d\n", 0));
+	printf("%d\n",printf("%++++++++--------d\n", -42));
+	printf("%d\n",printf("%--------++++++++d\n", 42));
+	printf("%d\n",printf("%--------++++++++d\n", 0));
+	printf("%d\n",printf("%--------++++++++d\n", -42));
+	printf("%d\n",printf("%++++++++########d\n", 42));
+	printf("%d\n",printf("%++++++++########d\n", 0));
+	printf("%d\n",printf("%++++++++########d\n", -42));
+	printf("%d\n",printf("%########++++++++d\n", 42));
+	printf("%d\n",printf("%########++++++++d\n", 0));
+	printf("%d\n",printf("%########++++++++d\n", -42));
+	printf("%d\n",printf("%++++++++00000000d\n", 42));
+	printf("%d\n",printf("%++++++++00000000d\n", 0));
+	printf("%d\n",printf("%++++++++00000000d\n", -42));
+	printf("%d\n",printf("%00000000++++++++d\n", 42));
+	printf("%d\n",printf("%00000000++++++++d\n", 0));
+	printf("%d\n",printf("%00000000++++++++d\n", -42));
+	printf("%d\n",printf("%++++++++        d\n", 42));
+	printf("%d\n",printf("%++++++++        d\n", 0));
+	printf("%d\n",printf("%++++++++        d\n", -42));
+	printf("%d\n",printf("%        ++++++++d\n", 42));
+	printf("%d\n",printf("%        ++++++++d\n", 0));
+	printf("%d\n",printf("%        ++++++++d\n", -42));
+	printf("%d\n",printf("%--------00000000d\n", 42));
+	printf("%d\n",printf("%--------00000000d\n", 0));
+	printf("%d\n",printf("%--------00000000d\n", -42));
+	printf("%d\n",printf("%00000000--------d\n", 42));
+	printf("%d\n",printf("%00000000--------d\n", 0));
+	printf("%d\n",printf("%00000000--------d\n", -42));
+	printf("%d\n",printf("%--------        d\n", 42));
+	printf("%d\n",printf("%--------        d\n", 0));
+	printf("%d\n",printf("%--------        d\n", -42));
+	printf("%d\n",printf("%        --------d\n", 42));
+	printf("%d\n",printf("%        --------d\n", 0));
+	printf("%d\n",printf("%        --------d\n", -42));
+	printf("%d\n",printf("%        00000000d\n", 42));
+	printf("%d\n",printf("%        00000000d\n", 0));
+	printf("%d\n",printf("%        00000000d\n", -42));
+	printf("%d\n",printf("%00000000        d\n", 42));
+	printf("%d\n",printf("%00000000        d\n", 0));
+	printf("%d\n",printf("%00000000        d\n", -42));
+	printf("%d\n",printf("%-+#0 d\n", 42));
+	printf("%d\n",printf("%-+#0 d\n", 0));
+	printf("%d\n",printf("%-+#0 d\n", -42));
+	printf("%d\n",printf("%-+# 0d\n", 42));
+	printf("%d\n",printf("%-+# 0d\n", 0));
+	printf("%d\n",printf("%-+# 0d\n", -42));
+	printf("%d\n",printf("%-+0 #d\n", 42));
+	printf("%d\n",printf("%-+0 #d\n", 0));
+	printf("%d\n",printf("%-+0 #d\n", -42));
+	printf("%d\n",printf("%-+0# d\n", 42));
+	printf("%d\n",printf("%-+0# d\n", 0));
+	printf("%d\n",printf("%-+0# d\n", -42));
+	printf("%d\n",printf("%+-#0 d\n", 42));
+	printf("%d\n",printf("%+-#0 d\n", 0));
+	printf("%d\n",printf("%+-#0 d\n", -42));
+	printf("%d\n",printf("%+- #0d\n", 42));
+	printf("%d\n",printf("%+- #0d\n", 0));
+	printf("%d\n",printf("%+- #0d\n", -42));
+	printf("%d\n",printf("%+- 0#d\n", 42));
+	printf("%d\n",printf("%+- 0#d\n", 0));
+	printf("%d\n",printf("%+- 0#d\n", -42));
+	printf("%d\n",printf("%+ -#0d\n", 42));
+	printf("%d\n",printf("%+ -#0d\n", 0));
+	printf("%d\n",printf("%+ -#0d\n", -42));
+	printf("%d\n",printf("%+ -0#d\n", 42));
+	printf("%d\n",printf("%+ -0#d\n", 0));
+	printf("%d\n",printf("%+ -0#d\n", -42));
+	printf("%d\n",printf("% +-#0d\n", 42));
+	printf("%d\n",printf("% +-#0d\n", 0));
+	printf("%d\n",printf("% +-#0d\n", -42));
+	printf("%d\n",printf("% +-0#d\n", 42));
+	printf("%d\n",printf("% +-0#d\n", 0));
+	printf("%d\n",printf("% +-0#d\n", -42));
+	printf("%d\n",printf("% -+#0d\n", 42));
+	printf("%d\n",printf("% -+#0d\n", 0));
+	printf("%d\n",printf("% -+#0d\n", -42));
+	printf("%d\n",printf("% -+0#d\n", 42));
+	printf("%d\n",printf("% -+0#d\n", 0));
+	printf("%d\n",printf("% -+0#d\n", -42));
+	printf("%d\n",printf("%- +#0d\n", 42));
+	printf("%d\n",printf("%- +#0d\n", 0));
+	printf("%d\n",printf("%- +#0d\n", -42));
+	printf("%d\n",printf("%- +0#d\n", 42));
+	printf("%d\n",printf("%- +0#d\n", 0));
+	printf("%d\n",printf("%- +0#d\n", -42));
+	printf("%d\n",printf("%- #+0d\n", 42));
+	printf("%d\n",printf("%- #+0d\n", 0));
+	printf("%d\n",printf("%- #+0d\n", -42));
+	printf("%d\n",printf("%- 0+#d\n", 42));
+	printf("%d\n",printf("%- 0+#d\n", 0));
+	printf("%d\n",printf("%- 0+#d\n", -42));
+	printf("%d\n",printf("%- #0+d\n", 42));
+	printf("%d\n",printf("%- #0+d\n", 0));
+	printf("%d\n",printf("%- #0+d\n", -42));
+	printf("%d\n",printf("%- 0#+d\n", 42));
+	printf("%d\n",printf("%- 0#+d\n", 0));
+	printf("%d\n",printf("%- 0#+d\n", -42));
+	printf("%d\n",printf("% -+#0d\n", 42));
+	printf("%d\n",printf("% -+#0d\n", 0));
+	printf("%d\n",printf("% -+#0d\n", -42));
+	printf("%d\n",printf("% -+0#d\n", 42));
+	printf("%d\n",printf("% -+0#d\n", 0));
+	printf("%d\n",printf("% -+0#d\n", -42));
+	printf("%d\n",printf("% -#+0d\n", 42));
+	printf("%d\n",printf("% -#+0d\n", 0));
+	printf("%d\n",printf("% -#+0d\n", -42));
+	printf("%d\n",printf("% -0+#d\n", 42));
+	printf("%d\n",printf("% -0+#d\n", 0));
+	printf("%d\n",printf("% -0+#d\n", -42));
+	printf("%d\n",printf("% -#0+d\n", 42));
+	printf("%d\n",printf("% -#0+d\n", 0));
+	printf("%d\n",printf("% -#0+d\n", -42));
+	printf("%d\n",printf("% -0#+d\n", 42));
+	printf("%d\n",printf("% -0#+d\n", 0));
+	printf("%d\n",printf("% -0#+d\n", -42));
+	printf("%d\n",printf("% +#-0d\n", 42));
+	printf("%d\n",printf("% +#-0d\n", 0));
+	printf("%d\n",printf("% +#-0d\n", -42));
+	printf("%d\n",printf("% +0-#d\n", 42));
+	printf("%d\n",printf("% +0-#d\n", 0));
+	printf("%d\n",printf("% +0-#d\n", -42));
+	printf("%d\n",printf("% -#+0d\n", 42));
+	printf("%d\n",printf("% -#+0d\n", 0));
+	printf("%d\n",printf("% -#+0d\n", -42));
+	printf("%d\n",printf("% -0+#d\n", 42));
+	printf("%d\n",printf("% -0+#d\n", 0));
+	printf("%d\n",printf("% -0+#d\n", -42));
+	printf("%d\n",printf("% -#0+d\n", 42));
+	printf("%d\n",printf("% -#0+d\n", 0));
+	printf("%d\n",printf("% -#0+d\n", -42));
+	printf("%d\n",printf("% -0#+d\n", 42));
+	printf("%d\n",printf("% -0#+d\n", 0));
+	printf("%d\n",printf("% -0#+d\n", -42));
+
+// ---------- LMC & precision ---------
+
+	printf("%d\n",printf("%3d", 42));
+	printf("%d\n",printf("%3d", 0));
+	printf("%d\n",printf("%3d", -42));
+	printf("%d\n",printf("%.7d", 42));
+	printf("%d\n",printf("%.7d", 0));
+	printf("%d\n",printf("%.7d", -42));
+	printf("%d\n",printf("%3.7d", 42));
+	printf("%d\n",printf("%3.7d", 0));
+	printf("%d\n",printf("%3.7d", -42));
+
+// --------- sizes ----------
+
+	printf("%d\n",printf("%hd\n", (short)42));
+	printf("%d\n",printf("%hd\n", (short)0));
+	printf("%d\n",printf("%hd\n", (short)-42));
+	printf("%d\n",printf("%hhd\n", (char)42));
+	printf("%d\n",printf("%hhd\n", (char)0));
+	printf("%d\n",printf("%hhd\n", (char)-42));
+	printf("%d\n",printf("%ld\n", (long)42));
+	printf("%d\n",printf("%ld\n", (long)0));
+	printf("%d\n",printf("%ld\n", (long)-42));
+	printf("%d\n",printf("%lld\n", (long long)42));
+	printf("%d\n",printf("%lld\n", (long long)0));
+	printf("%d\n",printf("%lld\n", (long long)-42));
+
+// ---------- repetitive conversions --------
+
+	printf("%d\n",printf("%d%d%d\n", 42, 0, -42));
+	printf("%d\n",printf("%d    %d           %d\n", 42, 0, -42));
+	printf("%d\n",printf("%#d% d%0d\n", 42, 0, -42));
+	printf("%d\n", printf("%ld%hd%d\n", (long)42, (short)0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+	printf("%d\n",printf("%d% d%0d\n", 42, 0, -42));
+
+// ----------- invalid printf ---------
+
+	printf("%d\n", printf("aha!\n", 42));
+	printf("%d\n", printf("%d\n"));
+	printf("%d\n", printf("%#d\n", 42));
+	printf("%d\n", printf("%h\n"));
+	printf("%d\n", printf("%yd\n", 42));
+	printf("%d\n", printf("%ll3.5\n", 42));
+	printf("%d\n", printf("%ld%hd%d\n", (short)42, (long)0, -42));
+	printf("%d\n", printf("%ld%hd%d\n", (short)42, 0, (long)-42));
+	return (0);
+}
